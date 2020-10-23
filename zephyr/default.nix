1f0b50b400dbec4c8fd732e19d6a61efb08015f2
Serhii Khoma <srghma@gmail.com>
2020-10-23 17:52:46 +0300

    feat: zephyr

--- zephyr/default.nix
+++ zephyr/default.nix
@@ -1,36 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  revisions = builtins.fromJSON (builtins.readFile ./revision.json);
-in pkgs.stdenv.mkDerivation rec {
-  name = "zephyr";
+  revisionDataFromFile = path:
+  let
+    revData = builtins.fromJSON (builtins.readFile path);
+  in
+  {
+    url = revData.url;
+    ref = "master";
+    rev = revData.rev;
+  };
+in
 
-  version = revisions.version;
-
-  src = if pkgs.stdenv.isDarwin
-    then pkgs.fetchurl { inherit (revisions.mac) url sha256; }
-    else pkgs.fetchurl { inherit (revisions.linux) url sha256; };
-
-  buildInputs = [ pkgs.gmp pkgs.zlib pkgs.ncurses5 ];
-
-  libPath = pkgs.lib.makeLibraryPath buildInputs;
-
-  dontStrip = true;
-
-  unpackPhase = ''
-    mkdir -p $out/bin
-    tar xf $src --strip 1 -C $out
-
-    ZEPHYR=$out/bin/zephyr
-    install -D -m555 -T $out/zephyr $ZEPHYR
-
-    chmod u+w $ZEPHYR
-    patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" --set-rpath ${libPath} $ZEPHYR
-    chmod u-w $ZEPHYR
-
-    mkdir -p $out/etc/bash_completion.d/
-    $ZEPHYR --bash-completion-script $ZEPHYR > $out/etc/bash_completion.d/zephyr-completion.bash
-  '';
-
-  dontInstall = true;
-}
+(import /home/srghma/projects/zephyr { nixpkgs = pkgs; }).zephyr
--- zephyr/update.sh
+++ zephyr/update.sh
@@ -1,30 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p nix curl jq
+#!nix-shell -i bash -p nix-prefetch-git
 
-# prelude
-script_dir=$(dirname "$(readlink -f "$BASH_SOURCE")")
-
-# config
-owner="coot"
-repo="zephyr"
-
-last_release_version=$(curl -s https://api.github.com/repos/$owner/$repo/releases/latest | jq --raw-output .tag_name)
-
-linux_url="https://github.com/$owner/$repo/releases/download/$last_release_version/x86_64-linux.tar.gz"
-echo "linux_url=$linux_url"
-mac_url="https://github.com/$owner/$repo/releases/download/$last_release_version/x86_64-osx.tar.gz"
-echo "mac_url=$mac_url"
-
-cat > "$script_dir/revision.json" <<EOF
-{
-  "version": "$last_release_version",
-  "linux": {
-    "url": "$linux_url",
-    "sha256": "$(nix-prefetch-url --quiet $linux_url)"
-  },
-  "mac": {
-    "url": "$mac_url",
-    "sha256": "$(nix-prefetch-url --quiet $mac_url)"
-  }
-}
-EOF
+SCRIPT_DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
+nix-prefetch-git https://github.com/coot/zephyr > "$SCRIPT_DIR/revision.json"
